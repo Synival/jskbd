a8ba01877f88bcf720c00ddc69f323f7270ea3d7
Simon Bielman <simon@baroque-creations.com>
2020-09-25 11:57:18 +0200

    Some refactoring, added comments, removed unecessary debug spam and leftover variables (HONK)

--- jskbd.js
+++ jskbd.js
@@ -1,21 +1,59 @@
+"use strict"
+
+//
+// Messages - could be localized, if you want!
+//
+
 const ERROR_NO_MIDI_DEVICES = "No MIDI input devices detected.";
 const ERROR_UNHANDLED_EVENT = "Unhandled event";
+
+//
+// Constant settings - maybe configurable in the future?
+//
 
 const MAX_VELOCITY = 127.00;
 const LOW_KEY = 21;
 const MAX_KEYS = 88;
+
+const CANVAS_WIDTH  = 60 * 8;
+const CANVAS_HEIGHT = MAX_KEYS * 8;
+
+//
+// Draw methods for notches, in order of interpolation between low
+// color and high color (0.00 .. 1.00)
+//
 
 const DRAW_COLOR1      = 'color1'
 const DRAW_COLOR1_DOTS = 'color1-dots'
 const DRAW_STRIPES     = 'stripes'
 const DRAW_COLOR2_DOTS = 'color2-dots'
 
-const CANVAS_WIDTH  = 60 * 8;
-const CANVAS_HEIGHT = MAX_KEYS * 8;
+//
+// References to elements
+//
 
 const gCanvas = document.getElementById('display');
 const gContext = gCanvas.getContext('2d');
+
+//
+// Internal structures
+//
+
+// All the keys tracked, with this object:
+//  {
+//      volume = [0.00 .. 1.00]
+//  }
+
 const gKeys = Array(MAX_KEYS).fill(null);
+
+// All the "notches", which indicate specific colors at specific volumes.
+// *Must* be defined with "volume" in low-to-high order.
+//
+// Object format:
+//  {
+//      volume = [0.00 .. 1.00]
+//      color: string for 'gContext.fillStyle'
+//  }
 
 const gNotches = [
     { volume: 0.00, color: '#000' }, // 0: Black
@@ -31,45 +69,75 @@ const gNotches = [
     { volume: 1.00, color: '#fff' }, // 10: White
 ];
 
+//
+// More constants - set using existing data.
+//
+
 const MAX_NOTCHES = gNotches.length;
 
-gCanvas.width  = CANVAS_WIDTH;
-gCanvas.height = CANVAS_HEIGHT;
+//
+// Mutable global variables
+//
 
-let gInputs = null;
-let gPos = 0;
-let gFrame = 0;
+let gInputs  = null;
+let gPos     = 0;
+let gFrame   = 0;
 let gRunning = false;
 
+// Initializes application. Always run first.
+function init() {
+    // Set the internal drawing size of our canvas.
+    gCanvas.width  = CANVAS_WIDTH;
+    gCanvas.height = CANVAS_HEIGHT;
+
+    // Activate MIDI.
+    navigator
+        .requestMIDIAccess()
+        .then(initMidi);
+}
+
+// Error message in a box.
 function errorAlert(message) {
     alert(message);
 }
 
-function errorLog(message) {
+// Error message in the console.
+function errorConsole(message) {
     console.error(message);
 }
 
+// Initializes MIDI features. Executed as a callback in requestMIDIAccess().
 function initMidi(access) {
     gInputs = access.inputs;
 
+    // Register all MIDI input devices.
+    // TODO: all input from all devices is routed to the same gKeys[] values.
+    // TODO: in other words, it breaks if you play two keyboards at once! :D
+    // TODO: fix that by tracking key states per input!
     let count = 0;
     for (let input of gInputs.values()) {
         registerInput(input);
         count++;
     }
+
+    // Bail if there's no MIDI input device.
+    // TODO: allow (un)plugging of devices at runtime.
+    // TODO: allow selecting input devices.
     if (count == 0) {
         errorAlert(ERROR_NO_MIDI_DEVICES);
         return;
     }
 
-    startDrawing();
+    // Start application!
+    startRunning();
 }
 
-let honk = null;
-
+// Interprets a single MIDI event from a device and acts accordingly.
+// TODO: should specify device if it doesn't already.
 function handleMidiMessage(message) {
     var data = message.data;
 
+    // data[0] is the MIDI command.
     switch (data[0]) {
         // Note on/off. Here's the good stuff.
         case 144: {
@@ -89,26 +157,33 @@ function handleMidiMessage(message) {
         case 254:
             break;
 
+        // Command not handled - complain in the console!
         default:
-            errorLog(ERROR_UNHANDLED_EVENT + ": " + data);
+            errorConsole(ERROR_UNHANDLED_EVENT + ": " + data);
             break;
     }
 }
 
+// Individual setup per MIDI input device.
+// TODO: track devices internally.
 function registerInput(input) {
     input.onmidimessage = handleMidiMessage;
 }
 
+// Converts the key from a MIDI event to the key number used in JSKBD.
 function midiKeyToArrayKey(midiKey) {
     if (midiKey < LOW_KEY || midiKey >= (LOW_KEY + MAX_KEYS))
         return null;
     return midiKey - LOW_KEY;
 }
 
+// Converts the velocity from a MIDI event to the internally-stored volume.
 function velocityToVolume(velocity) {
     return velocity / MAX_VELOCITY;
 }
 
+// Event triggered when a note stops.
+// TODO: probably needs the device
 function eventNoteOff(midiKey) {
     let key = midiKeyToArrayKey(midiKey);
     if (key == null)
@@ -116,13 +191,18 @@ function eventNoteOff(midiKey) {
     gKeys[key] = null;
 }
 
+// Event triggered when a note begins.
+// TODO: probably needs the device
 function eventNoteOn(midiKey, velocity) {
     let key = midiKeyToArrayKey(midiKey);
     if (key == null)
         return;
-    gKeys[key] = velocityToVolume(velocity);
+    gKeys[key] = {
+        volume: velocityToVolume(velocity),
+    };
 }
 
+// Draw a single animation frame.
 function animFrame() {
     clearLine();
     drawMidiLine();
@@ -130,6 +210,7 @@ function animFrame() {
     drawPositionLine();
 }
 
+// Clear the current cursor position before drawing the sequencer display.
 function clearLine() {
     if (gFrame % 10 == 9) {
         gContext.fillStyle = '#888';
@@ -140,21 +221,34 @@ function clearLine() {
     }
 }
 
+// Converts a key volume to an indicator of the "notch" with colors and style.
+// Return object format:
+//  {
+//      notch:  index of gNotch[]
+//      color1: color in gNotch[notch]
+//      color2: color in gNotch[notch + 1]
+//      interpolation: the [0.00 .. 1.00] percentage between 'color1' and 'color2'
+//      style:  style used for drawing. likely based on interpolation.
+//  }
 function volumeToNotch(volume) {
-    let notch, low, high, style, color1, color2;
+    let notch, low, high, style, color1, color2, interpolation;
 
-    console.log(volume);
+    // Search for the lower and upper notche that 'volume' belongs to.
     for (let i = 0; i < MAX_NOTCHES; i++) {
-
-        if (i == MAX_NOTCHES - 1) {
+        // Handle cases that are beyond the limits in gNotches[].
+        if ((i == MAX_NOTCHES - 1) ||
+            (i == 0 && i < gNotches[0].volume))
+        {
             notch  = i;
             low    = gNotches[i].volume;
             high   = MAX_VELOCITY;
             color1 = gNotches[i].color;
             color2 = gNotches[i].color;
             style  = DRAW_COLOR1;
+            interpolation = 0.00;
             break;
         }
+        // Handle all other cases (between or at notches).
         else if (volume >= gNotches[i + 0].volume &&
                  volume <  gNotches[i + 1].volume)
         {
@@ -168,9 +262,12 @@ function volumeToNotch(volume) {
         }
     }
 
-    if (style === undefined && low !== undefined && high !== undefined) {
-        let dist = (high - low);
-        let interpolation = (volume - low) / dist;
+    // Determine interpolation if not explicitly defined.
+    if (interpolation === undefined && low !== undefined && high !== undefined)
+        interpolation = (volume - low) / (high - low);
+
+    // Determine style and interpolation.
+    if (style === undefined && interpolation !== undefined) {
         style = (interpolation < 0.25) ? DRAW_COLOR1      :
                 (interpolation < 0.50) ? DRAW_COLOR1_DOTS :
                 (interpolation < 0.75) ? DRAW_STRIPES     :
@@ -181,27 +278,35 @@ function volumeToNotch(volume) {
         notch:  notch,
         color1: color1,
         color2: color2,
-        style:  style
+        interpolation: interpolation,
+        style:  style,
     };
 }
 
+// Draws a single line at the current position (gPos).
 function drawMidiLine() {
+    // Pre-calculations.
+    // TODO: can probably be calculated way earlier, but who cares! /shrug
     let pixelsPerNote = gCanvas.height / MAX_KEYS;
 
+    // Draw all keys.
     for (let i = 0; i < MAX_KEYS; i++) {
-        if (gKeys[i] == null || gKeys[i] <= 0)
+        // Skip keys that aren't pressed.
+        if (gKeys[i] == null)
             continue;
 
+        // Calculate line positions.
         let y1 = gCanvas.height - (pixelsPerNote * (i + 1));
         let y2 = gCanvas.height - (pixelsPerNote * (i + 0));
 
+        // Calculate position for small line in the middle of the big line.
         let split1 = y1 + (pixelsPerNote / 2.0);
         let split2 = split1 + 1;
 
-        let notch = volumeToNotch(gKeys[i]);
-        console.log(notch);
+        // Get all info for drawing.
+        let notch = volumeToNotch(gKeys[i].volume);
 
-        // TODO: actual drawing!
+        // Draw based on 'notch.style'.
         switch (notch.style) {
             case DRAW_COLOR1:
                 gContext.fillStyle = notch.color1;
@@ -233,32 +338,40 @@ function drawMidiLine() {
     }
 }
 
+// Moves the cursor forward. Run after every animation frame.
 function updatePosition() {
     gPos = (gPos + 1) % gCanvas.width;
     gFrame++;
 }
 
+// Draws the line indicating gPos.
 function drawPositionLine() {
     gContext.fillStyle = '#080';
     gContext.fillRect(gPos, 0, 1, gCanvas.height);
 }
 
-function runAnimFrame() {
+// Queues drawing the next frame and all subsequent frames.
+function queueAnimFrame() {
     requestAnimationFrame(function() {
         animFrame();
-        if (gCanvasRunning)
-            runAnimFrame();
+        if (gRunning)
+            queueAnimFrame();
     });
 }
 
-function startDrawing() {
-    gCanvasRunning = true;
+// Begins drawing the sequencer.
+function startRunning() {
+    if (gRunning === true)
+        return;
+    gRunning = true;
 
     drawPositionLine();
-    runAnimFrame();
+    queueAnimFrame();
 }
 
-// Activate MIDI.
-navigator
-    .requestMIDIAccess()
-    .then(initMidi);
+//
+// Program execution
+//
+
+// Initialize and run!
+init();
